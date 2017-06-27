568b938bd9623b3c3df4df95e35b8efbb14f34fe
Ekaterina <ekaterinashevyako@gmail.com>
2017-06-27 09:15:24 +0300

    Correction

--- SequenceCheck.java
+++ SequenceCheck.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class SequenceCheck {
   public static void main(String[] args) {
-    boolean check = true;
     Scanner in = new Scanner(System.in);
     System.out.println("Enter some sequence:");
     String input = in.nextLine();
@@ -14,22 +13,13 @@ public class SequenceCheck {
 	}
     } catch (NumberFormatException e) {
         System.err.println("Invalid number format.");
-        System.exit(0);
-    }
-    for (int i = 0; i < sequence.length; i++) {
-	System.out.println(sequence[i]);
+        return;
     }
     for (int i = 1; i < sequence.length; i++) {
-        if (sequence[i] >= sequence[i-1]) {
-            check = true;
-        } else {
-            System.out.println("Entered sequence is decreasing.");
-            check = false;
-            break;
-        }
+        if (sequence[i] < sequence[i-1]) {
+            System.out.println("Entered sequence is non-decreasing.");
+        } 
     } 
-    if (check == true) {
-        System.out.println("Entered sequence is non-decreasing.");
-    }		
+    System.out.println("Entered sequence is non-decreasing.");	
   }
 }
