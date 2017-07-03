b7622594f5b5d7b7a0962ee95648ea4cdbb79723
Ekaterina <ekaterinashevyako@gmail.com>
2017-07-03 20:14:33 +0300

    Changings with brackets

--- SequenceCheck.java
+++ SequenceCheck.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class SequenceCheck { 
 
   public static void main(String[] args) {
-    if (arg.length != 0) { 
+    if (args.length != 0) { 
       decreasing(args);
     } else {
       Scanner scanner = new Scanner(System.in);
@@ -14,9 +14,7 @@ public class SequenceCheck {
     }
   }
 
-
-
-  public static void decreaing(String[] parts) {    	
+  public static void decreasing(String[] parts) {    	
     int sequence[] = new int[parts.length];
     try {
       for (int i = 0; i < parts.length; i++){
@@ -35,5 +33,4 @@ public class SequenceCheck {
     System.out.println("Entered sequence is non-decreasing.");	
   }
 
-}
 }
